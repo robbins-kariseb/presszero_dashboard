1cec801d77845839e300fc7c683804aca898b8c1
Robbins Kariseb <robbins.kariseb@adaire.com>
2024-03-10 16:28:06 +0200

    availability

--- business/src/context/AppProvider.js
+++ business/src/context/AppProvider.js
@@ -60,7 +60,7 @@ const AppProvider = ({ children }) => {
     if (verification === undefined) {
       // Handle password Sign In
       const user = await USERS.signIn({password, username, accessKey: accessKey})
-      if (user.response === "successful" && user.userData && (user.userData.accessGroup === "super-user" || user.userData.accessGroup === "admin" || user.userData.accessGroup === "ghost-user")) {
+      if (user.response === "successful" && user.userData && (user.userData.accessGroup === "super-user" || user.userData.accessGroup === "admin" || user.userData.accessGroup === "owner" || user.userData.accessGroup === "ghost-user")) {
         localStorage.setItem('user_data', JSON.stringify(user))
         return true;
       } else {
